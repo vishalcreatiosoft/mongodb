e6453fe96cd11774b31c38bccf6a1be9e80e8dd3
vishalcreatiosoft <vishal.srivastava@creatiosoft.com>
2021-09-30 11:28:31 +0530

    commiting firstly

---
+++ promises.js
@@ -0,0 +1,13 @@
+const doWorkPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        //resolve([4,5,6,7])
+        reject('rejected..')
+    }, 2000);
+})
+
+
+doWorkPromise.then((result)=> {
+    console.log('Success!',result)
+}).catch((error) =>{
+    console.log('Error',error)
+})
